a2b9dc854a44b7b9d02a87ce6559e10387f7ea6e
Jamshed <jamshhed@vk.com>
2024-09-27 05:33:29 +0500

    Теперь тут работает всё, что надо, кроме проверк написания названия группы.

--- TeleMain.py
+++ TeleMain.py
@@ -1,38 +1,87 @@
 import telebot
 from telebot import types
-from main import Is_t_group
+from main import Is_t_group, Group_ID
 
-bot = telebot.TeleBot("7136769737:AAEZhLglJIQtGr88HEjqUW8sfx2lYglVHAo")
-'''@bot.message_handler(commands=['start'])
-def start_message(message):
-  bot.send_message(message.chat.id, Is_t_group(16), parse_mode="Markdown")'''
+bot = telebot.TeleBot("7931500372:AAF28kr9FZgftLFkBKHXmW7J3VqnGYKseEQ")
+
+"""@bot.message_handler(commands=['Group_list'])
+def group(message):
+  bot.send_message(message.chat.id, text="Напиши название своей группы (большими без побелов)".format(message.from_user))
+  group_name = message.upper()
+  GroupId = Group_ID(group_name)"""
+
+
 
 @bot.message_handler(commands=['start'])
-def start(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("Понедельник")
-    btn2 = types.KeyboardButton("Вторник")
-    btn3 = types.KeyboardButton("Среда")
-    btn4 = types.KeyboardButton("Четверг")
-    btn5 = types.KeyboardButton("Пятница")
-    btn6 = types.KeyboardButton("Суббота")
-    markup.add(btn1, btn2, btn3, btn4, btn5, btn6)
-    bot.send_message(message.chat.id, text="Привет, {0.first_name}! На какой день недели тебе выдать расписание?".format(message.from_user), reply_markup=markup)
+def main(message):
+  bot.send_message(message.chat.id, text="""Напиши название своей группы. На выбор: 
+1АС1 \t 1ИС1 \t 1С1 \t 1ТО1 \t1ТО2
+
+2АС1 \t 2ИС1 \t 2ИС2 \t 2ОС1 \t 2С1
+
+3АС1 \t 3ИС1 \t 3ИС2 \t 3ОС1 \t 3С1 \t 3Э1
+
+4ИС1 \t 4ОС1 \t 4С1 \t 4ИС2 \t 4АС1""".format(message.from_user))
+  bot.register_next_step_handler(message, getIdGroup);
+
+"""def start(message):
+  markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+  butn1 = types.KeyboardButton("1 курс")
+  butn2 = types.KeyboardButton("2 курс")
+  butn3 = types.KeyboardButton("3 курс")
+  butn4 = types.KeyboardButton("4 курс")
+  markup.add(butn1, butn2, butn3, butn4,)
+  bot.send_message(message.chat.id, text="Выбери свой курс".format(message.from_user), reply_markup=markup)
+  bot.register_next_step_handler(message, firstKurs);
+
+def firstKurs(message):
+  markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+  gr1 = types.KeyboardButton("1АС1")
+  gr2 = types.KeyboardButton("1ИС1")
+  gr3 = types.KeyboardButton("1С1")
+  gr4 = types.KeyboardButton("1ТО1")
+  gr5 = types.KeyboardButton("1ТО2")
+  markup.add(gr1, gr2, gr3, gr4, gr5)
+  bot.send_message(message.chat.id, text="Выбери группу".format(message.from_user),reply_markup=markup)"""
+
+
+
+def getIdGroup(message):
+  global GroupId
+  GroupId = Group_ID(message.text)
+  markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+  btn1 = types.KeyboardButton("Понедельник")
+  btn2 = types.KeyboardButton("Вторник")
+  btn3 = types.KeyboardButton("Среда")
+  btn4 = types.KeyboardButton("Четверг")
+  btn5 = types.KeyboardButton("Пятница")
+  btn6 = types.KeyboardButton("Суббота")
+  btn7 = types.KeyboardButton("Вся неделя")
+  btn8 = types.KeyboardButton("Сменить группу")
+  markup.add(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8)
+  bot.send_message(message.chat.id, text="На какой день недели тебе выдать расписание?".format(message.from_user), reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
 def func(message):
     if(message.text == "Понедельник"):
-        bot.send_message(message.chat.id, text=Is_t_group(16, 0), parse_mode="Markdown")
+        bot.send_message(message.chat.id, text=Is_t_group(GroupId, 0), parse_mode="Markdown")
     elif(message.text == "Вторник"):
-        bot.send_message(message.chat.id, text=Is_t_group(16, 1), parse_mode="Markdown")
+        bot.send_message(message.chat.id, text=Is_t_group(GroupId, 1), parse_mode="Markdown")
     elif (message.text == "Среда"):
-        bot.send_message(message.chat.id, text=Is_t_group(16, 2), parse_mode="Markdown")
+        bot.send_message(message.chat.id, text=Is_t_group(GroupId, 2), parse_mode="Markdown")
     elif (message.text == "Четверг"):
-        bot.send_message(message.chat.id, text=Is_t_group(16, 3), parse_mode="Markdown")
+        bot.send_message(message.chat.id, text=Is_t_group(GroupId, 3), parse_mode="Markdown")
     elif (message.text == "Пятница"):
-        bot.send_message(message.chat.id, text=Is_t_group(16, 4), parse_mode="Markdown")
+        bot.send_message(message.chat.id, text=Is_t_group(GroupId, 4), parse_mode="Markdown")
     elif (message.text == "Суббота"):
-        bot.send_message(message.chat.id, text=Is_t_group(16, 5), parse_mode="Markdown")
+        bot.send_message(message.chat.id, text=Is_t_group(GroupId, 5), parse_mode="Markdown")
+    elif (message.text == "Вся неделя"):
+        bot.send_message(message.chat.id, text=Is_t_group(GroupId, 6), parse_mode="Markdown")
+    elif (message.text == "Сменить группу"):
+      bot.send_message(message.chat.id, text="Уверен, что хочешь сменить группу? Нажми на кнопку ещё раз, если да")
+      bot.register_next_step_handler(message, main);
+    else:
+      bot.send_message(message.chat.id, text="Либо ты накосячил, либо я. Давай начнём с начала, нажми на /start")
 
 bot.infinity_polling()
--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 import requests
 
+def Group_ID(group_name):
+    Group_list = requests.get("https://urtk-journal.ru/api/groups/urtk")
+    Group_data = Group_list.json()
+    for i in range(0, len(Group_data)):
+        for n in range(0, len(Group_data[i]["groups"])):
+            if group_name == Group_data[i]["groups"][n]["name"]:
+                #print(Group_data[i]["groups"][n]["name"], Group_data[i]["groups"][n]["id"])
+                return Group_data[i]["groups"][n]["id"]
+
+def GroupChekName(group_name):
+    Group_list = requests.get("https://urtk-journal.ru/api/groups/urtk")
+    Group_data = Group_list.json()
+    for i in range(0, len(Group_data)):
+        for n in range(0, len(Group_data[i]["groups"])):
+            if group_name == Group_data[i]["groups"][n]["name"]:
+                #print(Group_data[i]["groups"][n]["name"], Group_data[i]["groups"][n]["id"])
+                return True
+            else:
+                pass
+    return False
 
 def Is_t_group(ID, ind):
     x = 0
-    strokes = []
+    if ind == 6:
+        strokes = ""
+    else:
+        strokes = []
     #Получаем доступ к объекту
     client = requests.get(f"https://urtk-journal.ru/api/schedule/group/{ID}")
     #Форматируем его под себя
@@ -14,7 +37,7 @@ def Is_t_group(ID, ind):
         for n in range(0, len(data["schedule"][i]["lessons"])):
             if "name" in data["schedule"][i]["lessons"][n]:
                 prepod = ""
-                enters = 15 - len(data["schedule"][i]["lessons"][n]["name"].split(" ")[0])
+                #enters = 15 - len(data["schedule"][i]["lessons"][n]["name"].split(" ")[0])
                 for j in range(0, len(data["schedule"][i]["lessons"][n]["name"].split(" "))):
                     if j == 0:
                         pass
@@ -23,29 +46,40 @@ def Is_t_group(ID, ind):
 
                 lenPrepod = 20 - len(prepod)
                 if x == 1 :
-                    les.append([data["schedule"][i]["lessons"][n]["number"], ". ",
+                    les.append(["*", data["schedule"][i]["lessons"][n]["number"], ".", "* ",
                                 data["schedule"][i]["lessons"][n]["name"].split(" ")[0],"\n", " " * 6, prepod,
                                 " " * lenPrepod, data["schedule"][i]["lessons"][n]["time"], "     ",
                                 data["schedule"][i]["lessons"][n]["office"], "\n"])
                 else:
                     les.append(["*", data["schedule"][i]["date"], " ==> ", data["schedule"][i]["day"],"*"])
-                    les.append([data["schedule"][i]["lessons"][n]["number"], ". ",
+                    les.append(["*", data["schedule"][i]["lessons"][n]["number"], ".", "* ",
                                 data["schedule"][i]["lessons"][n]["name"].split(" ")[0],"\n", " " * 6, prepod,
                                 " " * lenPrepod, data["schedule"][i]["lessons"][n]["time"], "     ",
                                 data["schedule"][i]["lessons"][n]["office"], "\n"])
                     x = 1
             else:
-                print("\n")
-                break
-        x = 0
-        stroke = ""
-        for i in range(0, len(les)):
-            for j in range(0, len(les[i])):
-                stroke = stroke + str(les[i][j])
-            stroke = stroke + "\n"
-        strokes.append(stroke  + "\n")
-    print(strokes[5])
-    return strokes[ind]
+                les.append("")
 
-Is_t_group(16, 2) #16 - моя группа
+        x = 0
+        if ind != 6:
+            stroke = ""
+            for i in range(0, len(les)):
+                for j in range(0, len(les[i])):
+                    stroke = stroke + str(les[i][j])
+                stroke = stroke + "\n"
+            strokes.append(stroke  + "\n")
+        else:
+            stroke = ""
+
+            for i in range(0, len(les)):
+                for j in range(0, len(les[i])):
+                    stroke = stroke + str(les[i][j])
+                stroke = stroke + "\n"
+            strokes = strokes + stroke + "\n"
+
+    if ind == 6:
+        return strokes
+    else:
+        return strokes[ind]
+
 
